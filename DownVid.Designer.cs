df9979a06bc0a4051395c11aa34c19830a30a1d3
saleh-salem <s.salem.1042000@gmail.com>
2024-06-17 02:16:21 +0300

    intial commit

---
+++ DownVid.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace dwonvid
+{
+    partial class DownVid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(273, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(435, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoEllipsis = true;
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(262, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 69);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(290, 154);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(418, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "A place of saving the video: ";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(135, 230);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(536, 33);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // DownVid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(787, 414);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "DownVid";
+            this.Text = "DownVid";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ DownVid.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using static System.Net.Http.HttpClient;
+
+
+namespace dwonvid
+{
+    public partial class DownVid : Form
+    {
+        public DownVid()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private static Random random = new Random();
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor= Color.Green;
+            var youtube = new YoutubeClient();
+            var VideoUrl = textBox1.Text;
+            var palce = textBox2.Text;
+            
+            //Ensure that url and place of saving is not null
+            if (string.IsNullOrWhiteSpace(VideoUrl) || string.IsNullOrWhiteSpace(palce))
+            {
+                button1.BackColor = Color.IndianRed;
+
+                MessageBox.Show("Please enter both the video URL and the save path.");
+                button1.BackColor = Color.Gray;
+
+                return;
+            }
+            //get the info aboute video
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(VideoUrl);
+            //choose the highst quality from the manifest file 
+            
+            
+           var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+
+            var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+           //save video in the path with the type
+            var savePath = Path.Combine(palce, $"video{RandomString(12)}.{streamInfo.Container}");
+            
+            await DownloadVideoWithProgressAsync(streamInfo, savePath);
+        }
+
+
+
+
+        private async Task DownloadVideoWithProgressAsync(IStreamInfo streamInfo, string savePath)
+        {
+            var httpClient = new HttpClient();
+            using (var response = await httpClient.GetAsync(streamInfo.Url, HttpCompletionOption.ResponseHeadersRead))
+            {
+                response.EnsureSuccessStatusCode();
+
+                var contentLength = response.Content.Headers.ContentLength ?? 0;
+                progressBar1.Maximum = 100;
+                progressBar1.Value = 0;
+
+                var inputStream = await response.Content.ReadAsStreamAsync();
+                using (var outputStream = File.Create(savePath))
+                {
+                    var buffer = new byte[81920];
+                    long totalBytesRead = 0;
+                    int bytesRead;
+
+                    while ((bytesRead = await inputStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await outputStream.WriteAsync(buffer, 0, bytesRead);
+                        totalBytesRead += bytesRead;
+                        int progress = (int)((totalBytesRead * 100) / contentLength);
+                        progressBar1.Value = progress;
+                    }
+                }
+            }
+
+            MessageBox.Show("Video downloaded successfully!");
+            this.Close();
+        }
+
+
+    }
+}
